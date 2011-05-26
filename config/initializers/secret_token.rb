e1b89dbb12eb423d8715c98c3f9efefca3310a44
Uri Gorelik <uri.gore@gmail.com>
2011-05-25 23:10:39 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JoyblindApp::Application.config.secret_token = '0eba861eed8002fe5e4dd1c82c68f02087ace201ec0038ab39fc9d53fd06f289fb6a6ab3dd47842b51c0d6ee89e5660b86b263b2e51e25c58da7bc75cbd4487f'
